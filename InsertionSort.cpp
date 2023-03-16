945ab2e997706e9bc272a825244405cee826bae1
Ahmedreda122 <ahmalolireda12@gmail.com>
2023-03-16 15:16:15 +0200

    Just a kickstart

---
+++ InsertionSort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename type, size_t size>
+void insertionSort(type (&arr)[size]);
+
+int main()
+{
+  int arr[7] = {10, 30, 25, 20, 60, 40, 50};
+  insertionSort(arr);
+
+  for (int i = 0; i < 6; i++)
+  {
+    cout << arr[i] << ", ";
+  }
+  cout << endl;
+}
+
+// 0    1    2    3  4  5  6 
+// 10, 30,  [30], 20, 60, 40, 50
+// i = 2
+// j = 0
+// current = 25
+template <typename type, size_t size>
+void insertionSort(type (&arr)[size])
+{
+  int currentIndex, j;
+
+  for (int i = 1; i < size; i++)
+  {
+    current = arr[i];
+    j = i - 1;
+
+    while (current < arr[j] && j >= 0)
+    {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+}
+
---
+++ SelectionSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+template <typename type, size_t size>
+void selectionSort(type (&arr)[size]);
+
+// 0  1  2  3  4  5  6 
+// 10 20 25 30 40 50 60
+// min = 50
+// i = 5, j = 6
+
+int main()
+{
+  int arr[7] = {10, 30, 25, 20, 60, 40, 50};
+  selectionSort(arr);
+
+  for (int i = 0; i < 6; i++)
+  {
+    cout << arr[i] << ", ";
+  }
+  cout << endl;
+}
+
+template <typename type, size_t size>
+void selectionSort(type (&arr)[size])
+{
+  int minindex = 0;
+ 
+  for(int i = 0; i < size - 1; i++)
+  {
+    minindex = i;
+    for (int j = i + 1; j < size; j++)
+    {
+      if (arr[j] < arr[minindex])
+      {
+        minindex = j;
+      }
+    }
+    if (minindex != i)
+      swap(arr[minindex], arr[i]);
+  }
+}
