05d0eabd981004a20ab6b36ce42db5fe8505897e
Ahmedreda122 <ahmalolireda12@gmail.com>
2023-03-21 17:47:23 +0200

    Creating main program

--- main.cpp
+++ main.cpp
@@ -9,22 +9,118 @@ void intializeArr(type (&arr)[size]);
 
 int main()
 {
+  int arr1[200];
+  intializeArr(arr1);
+  int size1 = sizeof(arr1) / sizeof(arr1[0]);
+  //time_t t = time(NULL);
+ // struct tm* time = localtime(&t);
+  int before1 = clock();
+  //sort::countingSort(arr1);
+  //sort::bubbleSort(arr1);
+
+  // sort::shellSort(arr1);
+  sort::mergeSort(arr1, size1);
+  // sort::quickSort(arr1, 0, size1 - 1);
+  clock_t time = clock() - before1;
+  sort::printarr(arr1);
+  //struct tm* time1 = localtime(&t);
+  printf ("This Algorithm take (%.5lf ms).\n", ((double)time/CLOCKS_PER_SEC) * 1000);
+
+
+  //int n = 5;
+  string choice;
   int arr[200];
   intializeArr(arr);
   int size = sizeof(arr) / sizeof(arr[0]);
-  //time_t t = time(NULL);
- // struct tm* time = localtime(&t);
- // int before = clock();
-  //sort::countingSort(arr);
- // sort::bubbleSort(arr);
+  int before;
 
-  // sort::quickSort(arr, 0, size - 1);
-  // sort::shellSort(arr);
-  sort::mergeSort(arr, size);
-  sort::printarr(arr);
-  //clock_t time = clock() - before;
-  //struct tm* time1 = localtime(&t);
-  printf ("It took me (%.20lf seconds).\n", time);
+  cout << "                   Welcome to Sorting Algorithms Program" << endl;
+  // cout << "Please Enter the size of array: " << endl;
+
+  // do
+  // {
+  //   cin >> n;
+  // }
+  // while (n <= 0);
+
+  // int arr[n];
+
+
+  while(true)
+  {
+    cout << "Please Choose one of the following Sorting Algorithms to perform it:\n1-Insertion Sort\n2-Selection Sort\n3-Shell Sort\n4-Counting Sort\n5-Bubble Sort\n6-Merge Sort\n7-Quick Sort\nEnter a number from 1 to 7 or Enter 0 to Exit:\n=>";
+    // Getting the input from the user
+    getline(cin, choice);
+    /*
+      Removing the spaces from the input string to the end of it, 
+      Then returning a pointer to the beginning of the removed spaces 
+      Then Erasing the content from if_remove returning pointer to the end of the string 
+    */ 
+    choice.erase(remove_if(choice.begin(), choice.end(), ::isspace), choice.end());
+    cin.ignore(0);
+    before = clock();
+    if (choice == "1")
+    {
+      return 1;
+      // sort::insertionSort(arr);
+      // clock_t time = clock() - before;
+      // sort::printarr(arr);
+      // printf ("This Algorithm take (%.8lf ms).\n", ((double)time/CLOCKS_PER_SEC) * 1000);
+    }
+    else if (choice == "2")
+    {
+      return 1;
+      // sort::selectionSort(arr);
+      // clock_t time = clock() - before;
+      // sort::printarr(arr);
+      // printf ("This Algorithm take (%.8lf ms).\n", ((double)time/CLOCKS_PER_SEC) * 1000);
+    }
+    else if (choice == "3")
+    {
+      sort::shellSort(arr);
+      clock_t time = clock() - before;
+      sort::printarr(arr);
+      printf ("This Algorithm take (%.8lf ms).\n", ((double)time/CLOCKS_PER_SEC) * 1000);
+    }
+    else if (choice == "4")
+    {
+      sort::countingSort(arr);
+      clock_t time = clock() - before;
+      sort::printarr(arr);
+      printf ("This Algorithm take (%.8lf ms).\n", ((double)time/CLOCKS_PER_SEC) * 1000);
+    }
+    else if (choice == "5")
+    {
+      sort::bubbleSort(arr);
+      clock_t time = clock() - before;
+      sort::printarr(arr);
+      printf ("This Algorithm take (%.8lf ms).\n", ((double)time/CLOCKS_PER_SEC) * 1000);
+    }
+    else if (choice == "6")
+    {
+      sort::mergeSort(arr, size);
+      clock_t time = clock() - before;
+      sort::printarr(arr);
+      printf ("This Algorithm take (%.8lf ms).\n", ((double)time/CLOCKS_PER_SEC) * 1000);
+    }
+    else if (choice == "7")
+    {
+      sort::quickSort(arr, 0, size - 1);
+      clock_t time = clock() - before;
+      sort::printarr(arr);
+      printf ("This Algorithm take (%.8lf ms).\n", ((double)time/CLOCKS_PER_SEC) * 1000);
+    }
+    else if (choice == "0")
+    {
+      cout << "Okay, Bye.";
+      return 0;
+    }
+    else
+    {
+      cout << "Wrong input, try again.";
+      return 1;
+    }
+  }
 }
 
 template <typename type, size_t size>
